21eee916841ad198d32bf1aac5227d349ef92b11
Raffaelus <rafael.araujo101@etec.sp.gov.br>
2021-06-09 18:58:33 -0300

    Aula 2

--- ReactJS/sistema_bancario/index.js
+++ ReactJS/sistema_bancario/index.js
@@ -1,28 +1,52 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
-    saldo;
-    rg;
+    _saldo;
+
+    sacar(valor){
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if (valor <= 0){
+            return;
+        }
+        this._saldo += valor;
+    }
+
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789
+
+const contaCorrenteRicardo = new ContaCorrente();
+
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo._saldo = 100;
+
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
+
+
+//----//
+const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 // const cliente3Nome = "Maria";
 // const cliente3CPF = 55522233309;
-// const cliente3Agencia = 1001;
-// const cliente3Saldo = 0;
 
-console.log(cliente1, cliente2);
+//console.log(cliente1, cliente2);
