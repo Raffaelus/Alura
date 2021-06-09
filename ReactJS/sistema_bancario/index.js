796f6dd40fe059a89acf98c491ad8c535b51b6ab
Raffaelus <rafael.araujo101@etec.sp.gov.br>
2021-06-09 19:51:10 -0300

    Aula 3 - Criação de módulos

--- ReactJS/sistema_bancario/index.js
+++ ReactJS/sistema_bancario/index.js
@@ -1,52 +1,25 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo;
-
-    sacar(valor){
-        if (this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if (valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const contaCorrenteRicardo = new ContaCorrente();
-
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo._saldo = 100;
-
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-console.log(contaCorrenteRicardo);
-
-
-//----//
 const cliente2 = new Cliente();
-
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-// const cliente3Nome = "Maria";
-// const cliente3CPF = 55522233309;
 
-//console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.agencia = 102;
+conta2.cliente = cliente2;
+
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
+
+console.log(conta2);
